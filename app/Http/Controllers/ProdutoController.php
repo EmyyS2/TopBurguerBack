9be2ffd88dca1330dbf5cf74f4da78bdd5caa0ef
EmyyS2 <miacontapcsenai@gmail.com>
2024-04-04 13:51:00 -0300

    cadastro de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -14,10 +14,23 @@ class ProdutoController extends Controller
                 'nome' => $produto->nome,
                 'preco' => $produto->preco,
                 'ingredientes' => $produto->ingredientes,
-                'image' => asset('storage/' . $produto->imagem)
+                'imagem' => asset('storage/' . $produto->imagem)
 
             ];
         });
         return response()->json($produtosComImagem);
     }
+    public function store(Request $request)
+    {
+        $produtoData = $request->all();
+        if ($request->hasFile('imagem')) {
+            $imagem = $request->file('imagem');
+            $nomeImagem = time() . '.' . $imagem->getClientOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+        $produto = Produto::create($produtoData);
+        return response()->json(['produto' => $produto], 201);
+    }
 }
+
